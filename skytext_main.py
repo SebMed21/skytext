778e8e7e285bb9d265283a982de051417b224c6c
SebMed21 <sebastian.medina301426@gmail.com>
2025-01-18 13:50:28 +0800

    updated main, utilized dicts for player stats and equipment

--- skytext_main.py
+++ skytext_main.py
@@ -1,13 +1,20 @@
-#player values           
-player_health       = 100
-player_magicka      = 100
-player_stamina      = 100
+player_stats = {
+    'player_name' : "Prisoner",
+    #player values           
+    'player_health'         : 100,
+    'player_magicka'        : 100,
+    'player_stamina'        : 100,  
+    'player_exp'            : 0,
+    'player_level'          : 1, 
     
-#player equipment
-player_right_hand   = ()
-player_left_hand    = ()
-player_helmet       = ()
-player_armor        = ()
-player_boots        = ()
-player_gloves       = ()
+}
 
+player_equipment = {
+    'player_right_hand'     : "",
+    'player_left_hand'      : "",
+    'player_helmet'         : "",
+    'player_body'           : "",
+    'player_gloves'         : "",    
+    'player_boots'          : "",
+    
+}
