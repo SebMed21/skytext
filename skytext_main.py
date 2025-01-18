c1e4da19912131a79fa88eb0f4bc42255ba488e2
SebMed21 <sebastian.medina301426@gmail.com>
2025-01-18 14:49:01 +0800

    updated main, added different menus for the player to interact with, added choice to access said menus, placeholder information and text

--- skytext_main.py
+++ skytext_main.py
@@ -5,7 +5,7 @@ player_stats = {
     'player_health'         : 100,
     'player_magicka'        : 100,
     'player_stamina'        : 100,  
-    'player_exp'            : 0,
+    'player_experience'     : 0,
     'player_level'          : 1,    
 }
 #dictionary for player's equipped gear
@@ -18,13 +18,56 @@ player_equipment = {
     'player_boots'          : "",
 }
 
-#a function that displays current player status
+#displays the current actions a user can do
+def action_screen():
+    print("")
+
+#displays the menu for inventory, skills, level up perks and map    
+def menu_screen():
+    print("")
+    print("( |    (1) Inventory    |   (2) Magic   |   (3) Perks   |   (4) Map     | )")
+    print("")
+    user_choice = input("Enter your choice: ")
+    
+    if user_choice == "1":
+        inventory_screen()
+    
+    elif user_choice == "2":
+        magic_screen()
+        
+    elif user_choice == "3":
+        perks_screen()
+        
+    elif user_choice == "4":
+        map_screen()
+        
+
+#displays the contents of the player's inventory
+def inventory_screen():
+    print("*show inventory here*")
+    
+#displays the magic spells the player can currently use
+def magic_screen():
+    print("*show magic here*")
+
+#displays the perks the player can take (character upgrades)
+def perks_screen():
+    print("*show perks here*")
+    
+#displays the map
+def map_screen():
+    print("*shows the map*")
+    
+    
+#displays current player status
 def display_status():
     print("")
     print(player_stats['player_name'])
     print("HP:", player_stats['player_health'], 
           "| MP:", player_stats['player_magicka'], 
           "| STA:", player_stats['player_stamina'])
+    print("EXP: ", player_stats['player_experience'],
+          "| LVL :", player_stats['player_level'])
     print("")
-    
-display_status()
+
+menu_screen()
