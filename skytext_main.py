e15baa6dda7ffafc042bc0b69448a42cc432a626
SebMed21 <sebastian.medina301426@gmail.com>
2025-01-18 14:19:10 +0800

    updated main, changed formatting, added notes

--- skytext_main.py
+++ skytext_main.py
@@ -1,3 +1,4 @@
+#dictionary for player status
 player_stats = {
     'player_name'           : "Prisoner",
     #player values           
@@ -7,7 +8,7 @@ player_stats = {
     'player_exp'            : 0,
     'player_level'          : 1,    
 }
-
+#dictionary for player's equipped gear
 player_equipment = {
     'player_right_hand'     : "",
     'player_left_hand'      : "",
@@ -17,11 +18,13 @@ player_equipment = {
     'player_boots'          : "",
 }
 
+#a function that displays current player status
 def display_status():
-    print(player_stats['player_name'])
     print("")
-    print("HP:", player_stats['player_health'])
-    print("MP:", player_stats['player_magicka'])
-    print("STA:", player_stats['player_stamina'])
+    print(player_stats['player_name'])
+    print("HP:", player_stats['player_health'], 
+          "| MP:", player_stats['player_magicka'], 
+          "| STA:", player_stats['player_stamina'])
+    print("")
     
 display_status()
