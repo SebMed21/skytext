c15fb779580f6a8bae76fe47c02df6438a261e2c
SebMed21 <sebastian.medina301426@gmail.com>
2025-01-18 13:10:29 +0800

    added player stats and a few values

--- skytext_main.py
+++ skytext_main.py
@@ -1,1 +1,13 @@
-print("Hello World :D")
+#player values           
+player_health       = 100
+player_magicka      = 100
+player_stamina      = 100
+    
+#player equipment
+player_right_hand   = ()
+player_left_hand    = ()
+player_helmet       = ()
+player_armor        = ()
+player_boots        = ()
+player_gloves       = ()
+
